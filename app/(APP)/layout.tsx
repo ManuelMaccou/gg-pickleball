6ddc5e275044d584b75ccf291af219be6580d71c
ManuelMaccou <93491644+ManuelMaccou@users.noreply.github.com>
2025-05-17 13:24:10 -0700

    styling changes

--- app/(APP)/layout.tsx
+++ app/(APP)/layout.tsx
@@ -1,6 +1,11 @@
 import "../globals.css";
+import type { Viewport } from 'next'
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+
+export const viewport: Viewport = {
+  themeColor: '#06287C',
+}
 
 export default async function RootLayout({
   children,
