e0144a6f31d0af35ef410225a6a581ede50df127
ManuelMaccou <93491644+ManuelMaccou@users.noreply.github.com>
2025-09-13 12:37:20 -0700

    Update page.tsx

--- app/(APP)/error/location-denied/page.tsx
+++ app/(APP)/error/location-denied/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useRouter } from 'next/navigation';
-import { Button, Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { Button, Card, Flex, Heading, Strong, Text } from '@radix-ui/themes';
 import { ArrowLeftIcon } from '@radix-ui/react-icons';
 import Link from 'next/link';
 
@@ -11,17 +11,22 @@ export default function NotCloseEnoughPage() {
   return (
     <Flex align="center" justify="center" px={'3'} style={{ minHeight: '100vh' }}>
       <Card style={{ maxWidth: 500, padding: '2rem' }}>
-        <Flex direction="column" gap="4" align="center">
-          <Heading>Location error</Heading>
-          <Text align="center" color="gray">
-            To log a match, this app needs permission to access your location. 
-            Please enable location services for this website in your browser settings and try again.
+        <Flex direction="column" gap="4">
+          <Heading align={'center'}>Location error</Heading>
+          <Text color="gray">
+            To log a match, this app needs permission to access your location.
           </Text>
-          <Button asChild size={'3'}>
-            <Link href={'mailto:play@ggpickleball.co'}>
-              Contact us
-            </Link>
-          </Button>
+          <Text color="gray" weight={'bold'}>iOS</Text>
+          <Text color="gray" mt={'-3'}>
+            Go to Settings &gt; Privacy & Security &gt; Location Services. 
+            Ensure it is on, and check the setting for your specific browser (e.g., Safari or Chrome)
+          </Text>
+          <Text color="gray" weight={'bold'}>Android</Text>
+          <Text color="gray" mt={'-3'}>
+            Go to Settings &gt; Location. Ensure it is on. 
+            You can also check app-specific permissions under Settings &gt; Apps &gt; 
+            [Your Browser] &gt; Permissions &gt; Location
+          </Text>
           <Button mt={'4'} size="3" variant='ghost' onClick={() => router.push('/play')}>
             <ArrowLeftIcon />
             Go Back
